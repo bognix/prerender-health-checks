ae3fd682d8c3519ef96e86c06913eaffc98ba07c
Bogna Knychala <bogna.ka@gmail.com>
2016-02-05 16:35:37 +0100

    use nose

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-import pytest
+import nose
 
 if __name__ == '__main__':
 
-    result = pytest.main()
+    result = nose.run()
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url='https://github.com/bognix/prerender-health-checks',
     author='Bogna "bognix" Knychala',
     install_requires=['pytest==2.8.7', 'requests==2.0.1',
-                      'virtualenv==1.11.6'],
+                      'virtualenv==1.11.6', 'nose==1.3.0'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     author_email='bogna.ka@gmail.com',
